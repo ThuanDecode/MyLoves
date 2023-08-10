b2591532582b9bab0494deedf231fe7228e62010
ThuanDecode <141533617+ThuanDecode@users.noreply.github.com>
2023-08-10 09:29:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yourDate = new Date("2016-10-30T02:10:26"),
+const yourDate = new Date("2022-12-14T02:10:26"),
 music = ['ido', 'noinaycoanh', 'nguoiamphu'];
 
 document.addEventListener('DOMContentLoaded', function(){
